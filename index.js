b3a35896037b15fd56ab3f83bbc7cd6abb9bab28
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-06-02 10:25:02 -0700

    deploying application for user testing

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const server = require('./server.js');
 const {PORT} = require('./secret.js');
 
-server.listen(PORT, () => {
-    console.log(`listening on Port: ${PORT}`)
+// server.listen(PORT, () => {
+//     console.log(`listening on Port: ${PORT}`)
+// });
+
+
+// Production
+var server = app.listen(process.env.PORT || 3000, () => {
+    var port = server.address().port;
+    console.log(`Express is working on port ${port}`)
 })
